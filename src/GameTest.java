d2a1ada153422bd6d46c1023e84b070fe8816550
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2018-08-21 13:35:09 +0700

    Add some test case

--- src/GameTest.java
+++ src/GameTest.java
@@ -15,5 +15,32 @@ public class GameTest extends TestCase {
        g.nextGeneration();
        assertEquals(0,g.getCellAlive());
     }
+    public void testCellOfTwoNeighbors(){
+        g.setAlive(0,0);
+        g.setAlive(1,0);
+        g.setAlive(0,1);
+        assertEquals(3,g.getCellAlive());
+        g.nextGeneration();
+        assertEquals(4,g.getCellAlive());
+    }
+    public void testCellOfThreeNeighbors(){
+        g.setAlive(0,0);
+        g.setAlive(1,0);
+        g.setAlive(0,1);
+        g.setAlive(1,1);
+        assertEquals(4,g.getCellAlive());
+        g.nextGeneration();
+        assertEquals(4,g.getCellAlive());
+    }
+    public void testCellOfMoreThanThreeNeighbors(){
+        g.setAlive(0,0);
+        g.setAlive(1,0);
+        g.setAlive(0,1);
+        g.setAlive(1,1);
+        g.setAlive(2,0);
+        assertEquals(5,g.getCellAlive());
+        g.nextGeneration();
+        assertEquals(3,g.getCellAlive());
+    }
 
 }
